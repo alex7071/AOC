afe1dbd736b6dd45155bb3d0350df154e16230a8
alex7071 <alex707@gmail.com>
2018-12-11 21:16:32 +0200

    Update Day3P1.py

--- Day3P1.py
+++ Day3P1.py
@@ -12,7 +12,7 @@ def read_strings():
 def main():
     textStringList = read_strings()
 
-    fabric = np.zeros((1000,1000), dtype=np.int16)
+    fabric = np.zeros((1100, 1100), dtype=np.int16)
 
     for line in textStringList:
         instr = re.findall('\d+',line)
@@ -23,8 +23,8 @@ def main():
         inches_height = int(instr[4])
 
         fabric_to_fill = fabric[column:column+inches_height, row:row+inches_width]
-        fabric_to_fill = (np.where(fabric_to_fill > 0, -1, value_to_fill))
-        fabric[column:column + inches_height, row:row + inches_width]=fabric_to_fill
+        fabric_to_fill = (np.where((fabric_to_fill > 0) | (fabric_to_fill == -1), -1, value_to_fill))
+        fabric[column:column + inches_height, row:row + inches_width] = fabric_to_fill
 
         # for w in range(0, inches_width):
         #     for h in range(0, inches_height):
